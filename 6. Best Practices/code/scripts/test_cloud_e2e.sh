d092c6400cf8ce434be2c6eb1ff8e05573405b49
Tejash Bansal <56173595+tejasjbansal@users.noreply.github.com>
2024-08-07 08:38:46 +0000

    updated script

--- 6. Best Practices/code/scripts/test_cloud_e2e.sh
+++ 6. Best Practices/code/scripts/test_cloud_e2e.sh
@@ -1,15 +1,18 @@
 export KINESIS_STREAM_INPUT="stg_ride_events-mlops-zoomcamp"
 export KINESIS_STREAM_OUTPUT="stg_ride_predictions-mlops-zoomcamp"
 
-# SHARD_ID=$(aws kinesis put-record \
-# --stream-name ${KINESIS_STREAM_INPUT} \
-# --partition-key 1 \
-# --data file://scripts/data.b64 \
-# --query 'ShardId' \
-# --output text)
+SHARD_ID=$(aws kinesis put-record  \
+        --stream-name ${KINESIS_STREAM_INPUT}   \
+        --partition-key 1  --cli-binary-format raw-in-base64-out  \
+        --data '{"ride": {
+            "PULocationID": 130,
+            "DOLocationID": 205,
+            "trip_distance": 3.66
+        },
+        "ride_id": 156}'  \
+        --query 'ShardId'
+    )
 
-# sleep 60
-export SHARD_ID="shardId-000000000001"
-SHARD_ITERATOR=$(aws kinesis get-shard-iterator --shard-id ${SHARD_ID} --shard-iterator-type TRIM_HORIZON --stream-name ${KINESIS_STREAM_OUTPUT} --query 'ShardIterator')
+#SHARD_ITERATOR=$(aws kinesis get-shard-iterator --shard-id ${SHARD_ID} --shard-iterator-type TRIM_HORIZON --stream-name ${KINESIS_STREAM_OUTPUT} --query 'ShardIterator')
 
-aws kinesis get-records --shard-iterator $SHARD_ITERATOR
+#aws kinesis get-records --shard-iterator $SHARD_ITERATOR
